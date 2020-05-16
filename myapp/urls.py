a03943590f8a9510edf5be34df24bd317b27ad5b
Aggrim Arora <aggrimarora@Aggrims-MacBook-Pro.local>
2020-05-16 14:39:08 -0400

    dynamic display of products

--- myapp/urls.py
+++ myapp/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
 
-    path('sales/forms', views.sales),
-
+    path('sales/forms', views.sales), 
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -15,22 +15,23 @@ def sales(request):
 
     if request.method == "POST":
         time = datetime.now()
-        sales_rep_id = int(request.POST.get('sales_rep'))
-        lemon = int(request.POST.get('lemonade_quan'))
-        orange = int(request.POST.get('orange_quan'))
-        sugary = int(request.POST.get('sugary_quan'))
-        ww = int(request.POST.get('whiskey_quan'))
-        if lemon != 0:
-            O = Order(ProductID = Product.objects.get(id=1), RepID = SalesRep.objects.get(id=sales_rep_id), Quantity = lemon, Total = 1.50 * lemon, Date = time)
-            O.save()
-        if orange != 0:
-            O = Order(ProductID = Product.objects.get(id=2), RepID = SalesRep.objects.get(id=sales_rep_id), Quantity = orange, Total = 2.00 * lemon, Date = time)
-            O.save()
-        if sugary != 0:
-            O = Order(ProductID = Product.objects.get(id=3), RepID = SalesRep.objects.get(id=sales_rep_id), Quantity = sugary, Total = 3.00 * lemon, Date = time)
-            O.save()
-        if ww != 0:
-            O = Order(ProductID = Product.objects.get(id=3), RepID = SalesRep.objects.get(id=sales_rep_id), Quantity = sugary, Total = 5.50 * lemon, Date = time)
-            O.save()
+        print(request.POST)
+        # sales_rep_id = int(request.POST.get('sales_rep'))
+        # lemon = int(request.POST.get('lemonade_quan'))
+        # orange = int(request.POST.get('orange_quan'))
+        # sugary = int(request.POST.get('sugary_quan'))
+        # ww = int(request.POST.get('whiskey_quan'))
+        # if lemon != 0:
+        #     O = Order(ProductID = Product.objects.get(id=1), RepID = SalesRep.objects.get(id=sales_rep_id), Quantity = lemon, Total = 1.50 * lemon, Date = time)
+        #     O.save()
+        # if orange != 0:
+        #     O = Order(ProductID = Product.objects.get(id=2), RepID = SalesRep.objects.get(id=sales_rep_id), Quantity = orange, Total = 2.00 * lemon, Date = time)
+        #     O.save()
+        # if sugary != 0:
+        #     O = Order(ProductID = Product.objects.get(id=3), RepID = SalesRep.objects.get(id=sales_rep_id), Quantity = sugary, Total = 3.00 * lemon, Date = time)
+        #     O.save()
+        # if ww != 0:
+        #     O = Order(ProductID = Product.objects.get(id=3), RepID = SalesRep.objects.get(id=sales_rep_id), Quantity = sugary, Total = 5.50 * lemon, Date = time)
+        #     O.save()
 
     return render(request, 'myapp/sales_form.html', context)
